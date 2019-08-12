d02792786f0f8b98042cf9bc2c90221821904108
mprattinger <mprattinger@outlook.com>
2019-08-12 22:39:01 +0200

    WIP

--- MPR-Booking-Calendar.php
+++ MPR-Booking-Calendar.php
@@ -2,5 +2,56 @@
 
 class MPR_BookingCalendar extends WP_Widget
 {
-    
+    public function __construct()
+    {
+        $widget_ops = array(
+            "classname" => "mpr_bookingcalendar",
+            "description" => "Dieses Widget soll gebuchte Zeiten in einem Kalender anzeigen",
+        );
+        parent::__construct("mpr_bookingcalendar", "MPR Booking Calendar", $widget_ops);
+    }
+
+    public function widget($args, $instance)
+    {
+        ?>
+        <div class="mpr-bc-container">
+            <div class="mpr-bc-selector">
+                <select id="mpr-bc-selector-month"></select>
+                <select id="mpr-bc-selector-year"></select>
+            </div>
+            <div class="mpr-bc-table">
+                <div class="mpr-bc-cell mpr-bc-header">
+                    M
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    D
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    M
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    D
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    F
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    S
+                </div>
+                <div class="mpr-bc-cell mpr-bc-header">
+                    S
+                </div>
+            </div>
+            <div class="mpr-bc-month-toggler">
+            <a href="#" id="mpr-bc-toggler-prev"><< Zurück</a><span>|</span><a href="#" id="mpr-bc-toggler-next">Weiter >></a>
+            </div>
+        </div>
+        <?php
+    }
+
+    public function form($instance){
+    }
+
+    function update($new_instance, $old_instance){
+    }
 }
--- mpr-booking-cal.php
+++ mpr-booking-cal.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
 * Plugin Name: Booking Calender with Google Cal
@@ -9,14 +8,26 @@
 * Author URI: http://yourwebsiteurl.com/
 **/
 
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
 require_once("MPR-Booking-Calendar.php");
 
 function load_scripts(){
+    $ftime = rand(1000, 9000);
 
+    $link = plugins_url( '/css/mpr-booking-calendar.css', __FILE__ );
+    wp_enqueue_style( 'mpr-booking-calendar-style', $link);
+
+    wp_register_script('mpr-booking-calendar', plugins_url( '/js/mpr-booking-calendar.js', __FILE__ ));
+
+    wp_enqueue_script( 'mpr-booking-calendar', plugins_url( '/js/mpr-booking-calendar.js', __FILE__ ), array(), $ftime, true);
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
 // register BEC_Weather_Widget
 add_action("widgets_init", function(){
-    register_widget("MPR-Booking-Calendar");
+    register_widget("MPR_BookingCalendar");
 });
